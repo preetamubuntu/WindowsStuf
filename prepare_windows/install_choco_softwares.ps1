5b3f9cad960f0a752cd2b91b16a3afefe51d89df
preetamubuntu <153458832+preetamubuntu@users.noreply.github.com>
2024-02-12 16:00:30 +0100

    Update install_choco_softwares.ps1

--- prepare_windows/install_choco_softwares.ps1
+++ prepare_windows/install_choco_softwares.ps1
@@ -1,10 +1,24 @@
+write-host "install choco"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 # set execution policy to remote
+write-host "set the execution policy back to remote signed"
 Set-ExecutionPolicy RemoteSigned -Confirm:$false
 # enable remote connection
+write-host "enable RDP"
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -Name fDenyTSConnections -Value 0
 # allow RDP
+write-host "open firewall for RDP"
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+# add deutsch Keyboard
+write-host "Add German keyboard"
+$keyboardlang = "0409:00000407"
+$lang = Get-WinUserLanguageList
+$lang[0].InputMethodTips.Add($keyboardlang) # set deutsch keyboard
+Set-WinUserLanguageList -LanguageList $lang
+Set-WinDefaultInputMethodOverride -InputTip $keyboardlang
+
+write-host "Install list of basic softwares"
+
 choco install dotnetfx -y
 choco install vscode -y
 choco install putty -y
