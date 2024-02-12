8531ec5b63886605237d26687d084048b97b70f3
preetamubuntu <153458832+preetamubuntu@users.noreply.github.com>
2024-02-12 14:24:45 +0100

    Update 01_basic_config.ps1

--- prepare_windows/01_basic_config.ps1
+++ prepare_windows/01_basic_config.ps1
@@ -1,4 +1,9 @@
+write-host "Lets first install NuGet package...."
+Install-PackageProvider -Name NuGet -Force nuget
+
 # install windows update module
+write-host "NuGet is installed, now lets install windows update"
 Install-Module -name PSWindowsUpdate -confirm:$false
+write-host "scan the packages and install updates"
 Get-WindowsUpdate -Install -AcceptAll -AutoReboot -confirm:$false
 
