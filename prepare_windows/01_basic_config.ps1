cac8f6207ccfd0771ac813e3cd948b90a172ca64
preetamzare2024 <156023525+preetamzare2024@users.noreply.github.com>
2024-02-15 14:52:04 +0100

    Update 01_basic_config.ps1

--- prepare_windows/01_basic_config.ps1
+++ prepare_windows/01_basic_config.ps1
@@ -4,11 +4,27 @@ e.g. Windows 2022 OS is patched todate
 #>
 
 write-host "Lets first install NuGet package...."
-Install-PackageProvider -Name NuGet -Force nuget
+Install-PackageProvider -Name NuGet -Force
 # !! it keeps asking for confirmation !!.
 
 # install windows update module
 write-host "NuGet is installed, now lets install windows update...."
-Install-Module -name PSWindowsUpdate -confirm:$false
+Install-Module -name PSWindowsUpdate -Force
 write-host "scan the packages and install updates......"
 Get-WindowsUpdate -Install -AcceptAll -AutoReboot -confirm:$false
+
+# create directory structure
+$parentdir = "c:\allthings"
+New-Item -Path $parentdir -Type Directory
+
+$folders = (
+    "from_rootca",
+    "to_rootca",
+    "prepare_windows",
+    "prepare_ad",
+    "prepare_subca",
+    "prepare_rootca"
+
+)
+
+$folders | ForEach-Object { New-Item -Path $parentdir -Name $_ -Type Directory }
