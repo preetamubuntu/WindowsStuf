866767ecc967e6aa1a97e4b2f0920aefdf2034ae
preetamzare2024 <156023525+preetamzare2024@users.noreply.github.com>
2024-02-17 08:34:32 +0100

    Update ca_variables.ps1

--- prepare_windows/CA/configure_inf/ca_variables.ps1
+++ prepare_windows/CA/configure_inf/ca_variables.ps1
@@ -1,11 +1,12 @@
-$keylength = 4096
-$domain = "zonkos.ict"
-$renewalperiod = 10
-# $OID = "1.2.3.4.1455.67.89.5"
-
+<#
+    Goals:
+        - create a CAPoliy.inf file which will change
+        key length, validity period and domain_name
+#>
 $inffile = get-content .\Template_CAPolicy.inf
-
 $inffile1 = $inffile -replace "RenewalKeyLength=4096", "RenewalKeyLength=$keylength"
-#, "becloud.io", $domain, "20", $renewalperiod
-$inffile2 = $inffile1 -replace "URL=http://crl.becloud.io/pki/cps.txt", "URL=http://crl.$domain/pki/cps.txt"
-$inffile2 -replace "RenewalValidityPeriodUnits=20", "RenewalValidityPeriodUnits=$renewalperiod" | set-content CAPolicy.inf -Confirm:$false
+$inffile2 = $inffile1 -replace "URL=http://crl.becloud.io/pki/cps.txt", `
+    "URL=http://crl.$domain_name/pki/cps.txt"
+$inffile2 -replace "RenewalValidityPeriodUnits=20", `
+    "RenewalValidityPeriodUnits=$rootcavalidityperiod" | `
+    set-content CAPolicy.inf -Confirm:$false
