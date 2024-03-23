efe989d32bc577ae703e42fb98334334eb4b4112
preetamzare2024 <156023525+preetamzare2024@users.noreply.github.com>
2024-03-23 11:58:21 +0100

    Update addsubctodomain.ps1

--- subca/addsubctodomain.ps1
+++ subca/addsubctodomain.ps1
@@ -1,7 +1,8 @@
 $domainadmin = "zonkos\fred" # change
-$adddomaincred = Read-Host -Prompt 'domain admin credentials' -AsSecureString
+$adddomaincred = Read-Host -Prompt 'domain admin credentials for Fred' -AsSecureString
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential `
     -ArgumentList $domainadmin, $adddomaincred
 Add-Computer -DomainName $domain_name `
     -Credential $Credential
 Restart-Computer
+start-sleep 22
